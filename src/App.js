0248c7bd98bb16efdc829880e48c3eb4ab0a3cf0
Teja-Srinivas509 <cheedallatejasrinivas@gmail.com>
2024-12-26 15:40:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import './App.css';
-import Homepage from './Pages/HomePage/Homepage';
-import Login from '../src/Pages/LoginPage/Login'
-import {Route,Routes} from 'react-router-dom'
-import Profile from './Pages/ProfilePage/Profile'
-import ManageProfile from './Components/ProfilePagecomponents/ManageProfile/ManageProfile';
-import AddProfile from './Components/ProfilePagecomponents/AddProfile/AddProfile';
-import LandingPage from './Pages/LandingPage/LandingPage';
-import Player from './Pages/Player/Player';
+import React from 'react';
+import ProductCRUD from './ProductCRUD';
 
 function App() {
-
   return (
     <div className="App">
-     <Routes>
-     <Route path="/login" element={<Login />} />
-     <Route path="/" element={<Homepage />} />
-     <Route path="/profile" element={<Profile />} />
-     <Route path="/manage-profiles" element={<ManageProfile />} />
-     <Route path="/add-profile" element={<AddProfile />} />
-     <Route path="/landing-page" element={<LandingPage />} />
-     <Route path="/player" element={<Player/>}/>
-     </Routes>
+      <ProductCRUD />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n';
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -13,9 +11,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter basename="/ReactClone">
-  <I18nextProvider i18n={i18n}>
     <App />
-  </I18nextProvider>
   </BrowserRouter>
 );
 
